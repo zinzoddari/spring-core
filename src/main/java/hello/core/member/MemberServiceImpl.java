3a7dfdbf47ae374c82543a5bcd4306e5b36a5869
zinzoddari <zinzoddari@gmail.com>
2022-12-04 01:19:52 +0900

    DIP를 지키기 위한 AppConfig를 생성 및 기존 코드 수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //추상화, 구체화 등에 의존 중인 상태. DIP 위반
+    private final MemberRepository memberRepository; //추상화, 구체화 등에 의존 중인 상태. DIP 위반
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
